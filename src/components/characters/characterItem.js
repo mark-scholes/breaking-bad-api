ce27ee4df2c337137360b19f086b7482b68f63e1
mark-scholes <innominatus15@outlook.com>
2020-07-10 16:35:42 +0100

    Created some cards from the api data. These are displayed in a grid. When the cards are hovered over they flip and some more information is shown on the rear of the card.

--- src/components/characters/characterItem.js
+++ src/components/characters/characterItem.js
@@ -1,7 +1,37 @@
 import React from "react";
 
 const characterItem = ({ item }) => {
-  return <div>{item.name}</div>;
+  const { portrayed, nickname, name, img, birthday, status } = item;
+  return (
+    <div className="card">
+      <div className="card-inner">
+        <div className="card-front">
+          <img src={img} alt="" />
+        </div>
+        <div className="card-back">
+          <h1>{name}</h1>
+          <ul>
+            <li>
+              <strong>Actor Name: </strong>
+              {portrayed}
+            </li>
+            <li>
+              <strong>NickName: </strong>
+              {nickname}
+            </li>
+            <li>
+              <strong>Birthday: </strong>
+              {birthday}
+            </li>
+            <li>
+              <strong>Status: </strong>
+              {status}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default characterItem;
